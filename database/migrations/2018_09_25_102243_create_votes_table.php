9bb0028b3dd2f1dd72dfa0df4e96f51d0c1ff9c0
Prajasmita <prajusisale777@gmail.com>
2018-09-25 21:22:01 +0530

    vote table migration

---
+++ database/migrations/2018_09_25_102243_create_votes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('votes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('email');
+            $table->integer('event_id');
+            $table->integer('event_p_id');
+            $table->integer('otp')->nullable();
+            $table->enum('vote',['0','1'])->default('0');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('votes');
+    }
+}
